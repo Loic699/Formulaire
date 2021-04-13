a4fca1e374a3467f2be8fa5159afa8a398ebfcb7
Loic699 <loicpimet@gmail.com>
2021-04-13 12:11:53 +0200

    first commit

---
+++ src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+
+
+
+export class SignUpComponent implements OnInit {
+  model: Order = new Order();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onSubmit() {
+    // form submitted
+    console.log(this.model);
+  }
+}
+export class Order {
+  title: string;
+  quantity: number;
+  date: Date;
+  contact: string;
+}
